86bac586e64cd112c086f8d139b6610b1add19cf
gyaneman <gyanexus7@gmail.com>
2015-01-16 00:52:16 +0900

    プラグインのボタンを実装

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 enchant();
- 
-var game = new Game(320, 320);
-game.preload('preload1.gif', 'preload2.wav');
-game.onload = function () {
-  // ここに処理を書いていきます。
-};
-game.start();
+var game;
+var scene_game;
+window.onload = function () {
+    game = new Game(480, 320);
+    game.onload = function () {
+        // 画像のプリロード
+        //game.preload('map0.png', 'chara3.png');
+
+        // メインメニューの表示
+        var scene_main_menu = new Scene();
+        scene_main_menu.backgroundColor = "pink";
+        var label_title = new Label("BREAK FACE");
+        label_title.x = 100;
+        label_title.y = 50;
+        scene_main_menu.addChild(label_title);
+        var btn_start = new Button("START");
+        btn_start.moveTo(150, 120);
+
+        scene_game = new Scene();
+        scene_game.backgroundColor = "green";
+
+        btn_start.ontouchstart = function(){
+            game.pushScene(scene_game);
+            this.text = "ボタンを押しました";
+        }
+        scene_main_menu.addChild(btn_start);
+        game.pushScene(scene_main_menu);
+    };
+    game.start();
+}
